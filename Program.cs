2d85224b27d9a95b1482d080c7a71fe48dc9d437
Софья Стрелкова <sstrelkova15@gmail.com>
2025-04-06 18:56:09 +0300

    Implement program launcher with menu for Notepad, Calculator, Paint, and custom applications

--- Program.cs
+++ Program.cs
@@ -1,68 +1,133 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
 
 namespace SystemProgramming
 {
     class Program
     {
-        private static void ListProcesses()
-        {
-             foreach (Process process in Process.GetProcesses())
-            {
-                Console.WriteLine($"ID: {process.Id}  Name: {process.ProcessName}");
-            }
-
-            Console.Write("get process by name: ");
-            var processByName = Console.ReadLine();
-            if (string.IsNullOrEmpty(processByName))
-            {
-                Console.WriteLine("Process name cannot be empty.");
-                return;
-            }
-
-            var list = Process.GetProcessesByName(processByName);
-            if (list.Length == 0)
-            {
-                Console.WriteLine($"No processes found with the name: {processByName}");
-                return;
-            }
-
-            foreach (var process in list)
-            {
-                Console.WriteLine($"-------------------------<Process ID: {process.Id}>-------------------------");
-                Console.WriteLine($"Process Name: {process.ProcessName}");
-                Console.WriteLine($"Process Start Time: {process.StartTime}");
-                Console.WriteLine($"Process Memory Size: {process.PrivateMemorySize64 / 1024} KB");
-                Console.WriteLine($"Process CPU Time: {process.TotalProcessorTime}");
-                Console.WriteLine($"Process Threads Count: {process.Threads.Count}");
-                Console.WriteLine($"Process Handle Count: {process.HandleCount}");
-                Console.WriteLine("\n");
-                ProcessModuleCollection modules = process.Modules;
-
-                foreach (ProcessModule module in modules)
-                {
-                    Console.WriteLine(
-                        $"-------------------------<Process ID: {process.Id} - Module Name: {module.ModuleName}>-------------------------");
-                    Console.WriteLine($"FileName: {module.FileName}");
-                    Console.WriteLine($"Base Address: {module.BaseAddress}");
-                    Console.WriteLine($"Entry Point Address: {module.EntryPointAddress}");
-                    Console.WriteLine($"File Version: {module.FileVersionInfo.FileVersion}");
-                    Console.WriteLine($"File Description: {module.FileVersionInfo.FileDescription}");
-                    Console.WriteLine($"File Company Name: {module.FileVersionInfo.CompanyName}");
-                    Console.WriteLine($"File Product Name: {module.FileVersionInfo.ProductName}");
-                    Console.WriteLine($"File Copyright: {module.FileVersionInfo.LegalCopyright}");
-                    Console.WriteLine($"File Product Version: {module.FileVersionInfo.ProductVersion}");
-                    Console.WriteLine("\n\n");
-                }
-            }
-        }
         private static void Main(string[] args)
         {
-            static void TimerCallback(Object o)
+            while (true)
             {
-                var interval = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine($"Youre list processes will refresh after {interval} seconds");
-                Timer timer = new Timer(TimerCallback, null, 0, interval * 1000);
-                ListProcesses();
+                Console.Clear();
+                DisplayMenu();
+
+                var choice = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case '1':
+                        LaunchNotepad();
+                        break;
+                    case '2':
+                        LaunchCalculator();
+                        break;
+                    case '3':
+                        LaunchPaint();
+                        break;
+                    case '4':
+                        LaunchCustomApp();
+                        break;
+                    case '0':
+                        Console.WriteLine("Exiting...");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+
+                Console.WriteLine("\nEnter to continue...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        static void DisplayMenu()
+        {
+            Console.WriteLine("=== Launching programs menu ===");
+            Console.WriteLine("1. Launch Notepad");
+            Console.WriteLine("2. Launch Calculator");
+            Console.WriteLine("3. Launch Paint");
+            Console.WriteLine("4. Choose custom program");
+            Console.WriteLine("0. Exit");
+            Console.Write("Vhoose option: ");
+        }
+
+        static void LaunchNotepad()
+        {
+            try
+            {
+                Console.WriteLine("Notepad launch...");
+                Process.Start("notepad.exe");
+                Console.WriteLine("Notepad запущено успішно");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Launch error for Notepad: {ex.Message}");
+            }
+        }
+
+        static void LaunchCalculator()
+        {
+            try
+            {
+                Console.WriteLine("Calculator launch...");
+                Process.Start("calc.exe");
+                Console.WriteLine("Calculator launched successfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Launch error for Calculator: {ex.Message}");
+            }
+        }
+
+        static void LaunchPaint()
+        {
+            try
+            {
+                Console.WriteLine("Paint launch...");
+                Process.Start("mspaint.exe");
+                Console.WriteLine("Paint launched successfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Launch error for Paint: {ex.Message}");
+            }
+        }
+
+        static void LaunchCustomApp()
+        {
+            Console.Write("Enter the .exe path: ");
+            string path = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                Console.WriteLine("Path cannot be empty");
+                return;
+            }
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("File does not exist");
+                return;
+            }
+
+            try
+            {
+                Console.WriteLine($"Program launch: {path}");
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = path,
+                    UseShellExecute = true
+                });
+                Console.WriteLine("Successfully launched");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error was occured during launch: {ex.Message}");
             }
         }
     }
